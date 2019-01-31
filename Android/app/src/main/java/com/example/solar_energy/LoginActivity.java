b095480128ee1b2fa243cdeefeeaf986f8853684
M <janu723@naver.com>
2019-01-31 17:08:36 +0900

    안들호이드 UI 완성

---
+++ Android/app/src/main/java/com/example/solar_energy/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.solar_energy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
