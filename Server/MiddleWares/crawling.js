4a8701d4f05e2f7961469999e60aace36411f184
M <janu723@naver.com>
2019-02-21 18:37:05 +0900

    크롤링 이미지 작업 테스트 다 완려

--- Server/MiddleWares/crawling.js
+++ Server/MiddleWares/crawling.js
@@ -1,14 +1,60 @@
 const cheerio = require('cheerio');
 const request = require('request');
+const fs = require('fs');
 
 const crawlingMiddleware = (req,res,next) =>{
     let page_num = 1
     let url = 'https://kr.enfsolar.com/pv/panel'
     request(url, (error,response, body) => {
         const $ = cheerio.load(body);
-        data = $('body > div.container > div > div.enf-pd-list > div.enf-pd-list-main > ul > li:nth-child(1) > div.enf-pd-list-main-li-right > table > tbody > tr:nth-child(1) > td.enf-yellow')
-        console.log(data.text().trim());
-        res.send(data.text());        
+        name = $('body > div.container > div > div.enf-pd-list >'+
+        ' div.enf-pd-list-main > ul > li:nth-child(1) '+
+        '> div.enf-pd-list-main-li-right > '+
+        'div.row.enf-row > div:nth-child(1) > a').text().split(':')
+        company = $('body > div.container > div > div.enf-pd-list > '+
+        'div.enf-pd-list-main > ul > li:nth-child(1) > '+
+        'div.enf-pd-list-main-li-right > div.row.enf-row > div:nth-child(1) > div.enf-blue').text().trim();
+        panelName = name[0].trim() + " : " + name[1].trim()
+        type = $('body > div.container > div > div.enf-pd-list > div.enf-pd-list-main > ul >'+
+        ' li:nth-child(1) > div.enf-pd-list-main-li-right > table >'+
+        ' tbody > tr:nth-child(1) >'+
+        ' td.enf-yellow').text().trim()
+        percent = $('body > div.container > div > div.enf-pd-list > '+
+        'div.enf-pd-list-main > ul > li:nth-child(1) > div.enf-pd-list-main-li-right'+
+        ' > table > tbody > tr:nth-child(1)'+
+        ' > td.enf-blue').text().trim()
+        range = $('body > div.container > div > div.enf-pd-list >'+
+        ' div.enf-pd-list-main > ul > li:nth-child(1) >'+
+        ' div.enf-pd-list-main-li-right > table > tbody '+
+        '> tr.enf-grey-back > td:nth-child(2)').text().trim()
+        weight = $('body > div.container > div > div.enf-pd-list '+
+        '> div.enf-pd-list-main > ul > li:nth-child(1) '+
+        '> div.enf-pd-list-main-li-right > table '+
+        '> tbody > tr.enf-grey-back > td:nth-child(4)').text().trim()
+        contry = $('body > div.container > div > div.enf-pd-list '+
+        '> div.enf-pd-list-main > ul > li:nth-child(1) > '+
+        'div.enf-pd-list-main-li-right > table > tbody '+
+        '> tr:nth-child(3) > td:nth-child(2)').text().trim()
+        appearance = $('body > div.container > div > div.enf-pd-list '+
+        '> div.enf-pd-list-main > ul > li:nth-child(1) > '+
+        'div.enf-pd-list-main-li-right > table > tbody '+
+        '> tr:nth-child(3) > td:nth-child(4)').text().trim()
+        imgUrl = $('body > div.container > div > div.enf-pd-list > '+
+        'div.enf-pd-list-main > ul > li:nth-child(1) '+
+        '> div.enf-pd-list-main-li-left').find('img').attr('data-original')
+        let imgOption = {
+            method:"GET",
+            url:imgUrl,
+            encoding: null
+        }
+        console.log(imgUrl);
+        
+        request(imgOption).pipe(fs.createWriteStream('./imgPanel/panel.jpg'));
+        let data = {name:panelName,type:type,percent:percent,range:range,weight:weight
+            ,contry:contry,appearance:appearance,company:company}
+           
+        console.log(data);
+        res.send(data);        
     })
 }
 
--- Server/Routes.js
+++ Server/Routes.js
@@ -1,7 +1,9 @@
 let userRotue = require('./Router/UserRoute');
 let panelRoute = require('./Router/PanelRoute');
+let crawlRoute = require('./MiddleWares/crawling');
 
 module.exports = (app) => {
     app.use('/api/User',userRotue());
     app.use('/api/Panel',panelRoute());
+    app.use('/crawldata',crawlRoute);
 }
