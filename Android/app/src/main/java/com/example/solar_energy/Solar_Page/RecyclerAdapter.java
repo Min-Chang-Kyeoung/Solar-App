45e96136d4a74612db6ce751c2784908e1c2d87d
Pyeon Sangjin <vustkd157157@naver.com>
2019-02-28 18:16:49 +0900

    add network function:

--- Android/app/src/main/java/com/example/solar_energy/Solar_Page/RecyclerAdapter.java
+++ Android/app/src/main/java/com/example/solar_energy/Solar_Page/RecyclerAdapter.java
@@ -1,11 +1,5 @@
 package com.example.solar_energy.Solar_Page;
 
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -13,55 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-
 import com.example.solar_energy.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
-    Context context;
-    List<Solar_Item> items;
-    int item_layout;
+    private List<Solar_Item> items;
 
-    public RecyclerAdapter(Context context, List<Solar_Item> items, int item_layout) {
-        this.context = context;
+    public RecyclerAdapter( List<Solar_Item> items) {
         this.items = items;
-        this.item_layout = item_layout;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_solar, null);
-        return new ViewHolder(v);
-    }
-
-
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        final Solar_Item item = items.get(position);
-        Drawable drawable = ContextCompat.getDrawable(context, item.getImage());
-        holder.image.setImageDrawable(drawable);
-        holder.title.setText(item.getTitle());
-        holder.company.setText(item.getCompany());
-        holder.cardview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, item.getTitle(), Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(context,DetailPanelActivity.class);
-                intent.putExtra("img",item.getImage());
-                intent.putExtra("title",item.getTitle());
-                intent.putExtra("company",item.getCompany());
-                context.startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public int getItemCount() {
-        return this.items.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -78,4 +33,44 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
             cardview = (CardView) itemView.findViewById(R.id.cardview);
         }
     }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_solar, null);
+        //set the view's size, margins, paddings and layout parameters
+
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Solar_Item item = items.get(position);
+
+       // Drawable drawable = ContextCompat.getDrawable(context, item.getImage());
+       // holder.image.setImageDrawable(drawable);
+        Picasso.get().load(item.getImage()).into(holder.image);
+        holder.title.setText(item.getTitle());
+        holder.company.setText(item.getCompany());
+
+/*
+        holder.cardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, item.getTitle(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,DetailPanelActivity.class);
+                intent.putExtra("img",item.getImage());
+                intent.putExtra("title",item.getTitle());
+                intent.putExtra("company",item.getCompany());
+                context.startActivity(intent);
+            }
+        });
+        */
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.items.size();
+    }
+
+
 }
